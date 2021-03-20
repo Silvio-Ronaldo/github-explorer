5b4efe300400aac96ba63b1053d8a63a1c3f7efb
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-03-20 18:50:06 -0300

    Estilizando dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-	return <h1>Olá Dashboard</h1>;
+	return (
+		<>
+			<img src={logo} alt="Logo Github Explorer" />
+			<Title>Explore repositórios no Github</Title>
+
+			<Form>
+				<input type="text" placeholder="Digite o nome do repositório" />
+				<button type="submit">Pesquisar</button>
+			</Form>
+
+			<Repositories>
+				<a href="teste">
+					<img
+						src="https://avatars.githubusercontent.com/u/48893927?s=460&u=5a414a7b65386954ac45e948d653258d980cc7dd&v=4"
+						alt="Silvio Ronaldo"
+					/>
+
+					<div>
+						<strong>Silvio-Ronaldo/StudiHub-Web</strong>
+						<p>
+							Site do StudiHub, projeto de educação para auxiliar
+							estudantes universitários.
+						</p>
+					</div>
+
+					<FiChevronRight size={20} />
+				</a>
+			</Repositories>
+		</>
+	);
 };
 
 export default Dashboard;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Title = styled.h1`
+	font-size: 36px;
+	color: grey;
+
+	margin-top: 30px;
+	max-width: 400px;
+	line-height: 56px;
+`;
+
+export const Form = styled.form`
+	margin-top: 25px;
+	max-width: 700px;
+	display: flex;
+
+	input {
+		flex: 1;
+		padding: 0 24px;
+		height: 70px;
+		border: 0;
+		border-radius: 5px 0 0 5px;
+		color: #3a3a3a;
+
+		&::placeholder {
+			color: #a8a8b3;
+		}
+	}
+
+	button {
+		width: 210px;
+		height: 70px;
+		background-color: #04d361;
+		border-radius: 0 5px 5px 0;
+		border: 0;
+		color: #fff;
+		font-weight: bold;
+		transition: background-color 0.2s;
+
+		&:hover {
+			background: ${shade(0.2, '#04d361')};
+		}
+	}
+`;
+
+export const Repositories = styled.div`
+	margin-top: 60px;
+	max-width: 700px;
+
+	a {
+		background-color: #fff;
+		display: flex;
+		text-decoration: none;
+		padding: 20px;
+		align-items: center;
+		border-radius: 5px;
+		transition: transform 0.2s;
+
+		&:hover {
+			transform: translateX(10px);
+		}
+
+		& + a {
+			margin-top: 16px;
+		}
+
+		img {
+			border-radius: 50%;
+			width: 64px;
+			height: 64px;
+		}
+
+		div {
+			margin-left: 16px;
+
+			strong {
+				font-size: 20px;
+				color: #3d3d4d;
+			}
+
+			p {
+				font-size: 18px;
+				color: #a8a8b3;
+				margin-top: 4px;
+			}
+		}
+
+		svg {
+			margin-left: auto;
+			color: #cbabd6;
+		}
+	}
+`;
